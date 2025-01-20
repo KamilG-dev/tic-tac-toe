b84b83abea4fb516cde9860ff419bb56f79255ee
KamilG-dev <gibalakamil04@gmail.com>
2025-01-20 22:11:23 +0100

    basic game class with tests

---
+++ server/game.py
@@ -0,0 +1,77 @@
+from typing import List
+
+winning_combinations = [
+    [(0, 0), (0, 1), (0, 2)],
+    [(1, 0), (1, 1), (1, 2)],
+    [(2, 0), (2, 1), (2, 2)],
+    # columns
+
+    [(0, 0), (1, 0), (2, 0)],
+    [(0, 1), (1, 1), (2, 1)],
+    [(0, 2), (1, 2), (2, 2)],
+    # rows
+
+    [(0, 0), (1, 1), (2, 2)],
+    [(0, 2), (1, 1), (2, 0)],
+    # diagonals
+]
+
+class TicTacToe:
+    _board: List[List[int]]
+    """
+    0 - empty, 1 - O, 2 - X
+    """
+    _o_to_move: bool
+    _game_over: bool
+    _size: int
+
+    def __init__(self):
+        self._board = [[0 for _ in range(3)] for _ in range(3)]
+        self._o_to_move = True
+        self._game_over = False
+        self._size = 3
+        
+    @property
+    def board(self) -> List[List[int]]:
+        return self._board
+    
+    @property
+    def o_to_move(self) -> bool:
+        return self._o_to_move
+    
+    def restart(self):
+        self._board = [[0 for _ in range(3)] for _ in range(3)]
+        self._o_to_move = True
+
+
+    def move(self, x: int, y: int) -> int:
+        """
+        place a move on the board, return 1 if success and 0 if failed
+        """
+        if x < 0 or x >= self._size or y < 0 or y >= self._size:
+            return 0
+        
+        if self._game_over:
+            return 0
+        
+        if self._board[x][y] != 0:
+            return 0
+        
+        
+        self._board[x][y] = 1 if self._o_to_move else 2
+        self._o_to_move = not self._o_to_move
+
+        return 1
+    
+    def check_winner(self) -> int:
+        """
+        return 0 if no winner, 1 if O won, 2 if X won
+        """
+        for combination in winning_combinations:
+            if all(self._board[x][y] == 1 for x, y in combination):
+                self._game_over = True
+                return 1
+            if all(self._board[x][y] == 2 for x, y in combination):
+                self._game_over = True
+                return 2
+        return 0
---
+++ server/tests/test_tic_tac_toe.py
@@ -0,0 +1,71 @@
+from game import TicTacToe
+
+def test_init():
+    game = TicTacToe()
+    assert game.board == [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+    assert game._o_to_move == True
+    assert game._game_over == False
+
+def test_move():
+    game = TicTacToe()
+    assert game.o_to_move == True
+    assert game.move(0, 0) == 1
+    assert game.board[0][0] == 1
+
+    assert game.o_to_move == False
+    assert game.move(0, 0) == 0
+    assert game.board[0][0] == 1
+
+    assert game.move(0, 1) == 1
+    assert game.board[0][1] == 2
+    assert game.o_to_move == True
+    assert game.move(0, 1) == 0
+
+    assert game.move(3, 0) == 0
+    assert game.o_to_move == True
+
+def test_restart():
+    game = TicTacToe()
+    game.move(0, 0)
+
+    game.restart()
+
+    assert game.board == [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+    assert game.o_to_move == True
+    assert game._game_over == False
+
+def test_win_rows():
+    game = TicTacToe()
+    game.move(0, 0)
+    game.move(1, 0)
+    game.move(0, 1)
+    game.move(1, 1)
+    game.move(0, 2)
+    assert game.check_winner() == 1
+    assert game._game_over == True
+
+
+def test_win_diagonals():
+    game = TicTacToe()
+
+    game.move(1, 0)
+    game.move(0, 0)
+    game.move(2, 0)
+    game.move(1, 1)
+    game.move(0, 1)
+    game.move(2, 2)
+    assert game.check_winner() == 2
+    assert game._game_over == True
+
+def test_win_columns():
+    game = TicTacToe()
+    game.move(0, 0)
+    game.move(0, 1)
+    game.move(1, 0)
+    game.move(1, 1)
+    game.move(2, 0)
+
+    assert game.check_winner() == 1
+    assert game._game_over == True
+
+
