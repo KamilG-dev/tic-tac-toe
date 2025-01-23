5dc69e21b732bb6a24f2a189f527a878fe20f0a0
KamilG-dev <gibalakamil04@gmail.com>
2025-01-23 07:12:31 +0100

    flask socketio and env

--- server/app.py
+++ server/app.py
@@ -1,10 +1,26 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_socketio import SocketIO
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
+socketio = SocketIO(app)
+
+@socketio.on('connect')
+def connect():
+    print('Client connected')
+
+@socketio.on('event')
+def handle_my_custom_event(json):
+    print('received data: ' + str(json))
+
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run()
+    socketio.run(app, debug=True)
